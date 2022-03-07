08a01249466429aa9e64534a56cbc4a3dfb0d4e9
ali kazemi <alikazemidev@gmail.comi>
2022-03-07 23:54:14 +0330

    flutter amir ahamad week 3 layout challen

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+            body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.red,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.black,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.orange,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.yellow,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.indigo,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
